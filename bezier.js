e3c919e72282d3d65eae4c50ea24dc3e0b9f7a20
Salih Cebeci <salihcebeci@gmail.com>
2021-08-28 02:30:37 +0300

    a

--- bezier.js
+++ bezier.js
@@ -2,7 +2,7 @@ var counter = 0;
 
 function setup() {
   createCanvas(800, 800);
-  frameRate(60);
+  frameRate(20);
 }
 
 function draw() {
@@ -13,16 +13,29 @@ function draw() {
 
   var r = Math.random();
   if (r < 0.25)
+  {
     fill(200, 0, 0, 60);
-  else if (r < 0.5)
-    fill(255, 255, 255, 60);
-  else if (r < 0.75)
+    stroke(200, 0, 0, 25);
+    strokeWeight(10);
+  }
+  else if (r < 0.5){
+    fill(0, 200, 0, 60);
+    stroke(0, 200, 0, 25);
+    noStroke();
+  }
+  else if (r < 0.75){
     fill(200, 200, 0, 60);
-  else
+    stroke(200, 200, 0, 25);
+    strokeWeight(10);
+  }
+  else{
     fill(0, 0, 200, 60);
+    stroke(0, 0, 200, 25);
+    strokeWeight(10);
+  }
 
 
-  noStroke();
+
   bezier(rnd(), rnd(), rnd(), rnd(), rnd(), rnd(), rnd(), rnd());
 
 }
